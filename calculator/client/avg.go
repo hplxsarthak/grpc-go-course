fce07a776af961aab4e6dbe1e96b4cde3fc7bc09
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-22 15:18:48 +0530

    Add client boiler plate for Avg

---
+++ calculator/client/avg.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/hplxsarthak/grpc-go-course/calculator/proto"
+)
+
+func doAvg (c pb.CalculatorServiceClient) {
+	log.Println("doAvg was invoked")
+
+	stream, err := c.Avg(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while opening the stream %v\n", err)
+	}
+
+	numbers := []int64 {3,5,9,54,23}
+
+	for _, number := range numbers {
+		log.Printf("Sending number: %d\n", number)
+		stream.Send(&pb.AvgRequest{
+			Number: number,
+		})
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error while receiving response from Avg %v\n", err)
+	}
+
+	log.Printf("Avg: %f\n", res.Result)
+}
