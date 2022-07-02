edb26b222c9492831c883c3fc0010b146664a511
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-07-02 13:39:14 +0530

    Add opts to client also to make it work properly

--- greet/client/main.go
+++ greet/client/main.go
@@ -5,14 +5,29 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
+	"google.golang.org/grpc/credentials"
+	
 	pb "github.com/hplxsarthak/grpc-go-course/greet/proto"
 )
 
 var addr string = "localhost:50051"
 func main() {
-	conn,err := grpc.Dial(addr,grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true // if not needed tls change to false
+	opts := []grpc.DialOption{}
+
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds,err := credentials.NewClientTLSFromFile(certFile,"")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	conn,err := grpc.Dial(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
@@ -26,6 +41,6 @@ func main() {
 	// doGreetManyTimes(c)
 	// doLongGreet(c)
 	// doGreetEveryone(c)
-	// doGreetWithDeadline(c,5 * time.Second)
-	doGreetWithDeadline(c, 1 * time.Second)
+	doGreetWithDeadline(c,5 * time.Second)
+	// doGreetWithDeadline(c, 1 * time.Second)
 }
